9f65eccd4339e2ba1b5ba8b198c510561de211df
WooSung Choi <johnwschoi@gmail.com>
2018-04-07 12:26:39 -0700

    simple nextjs tutorial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ssrnextjs
+NextJS tutorial link
+
+https://medium.freecodecamp.org/discover-next-js-and-write-server-side-react-apps-the-easy-way-cc920dea2d9d
---
+++ pages/post.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+const Post = (props) => (
+  <li>
+    <Link as={`/${props.slug}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+export default () => (
+  <div>
+    <h2>My blog</h2>
+    <ul>
+      <li>
+        <Post slug="yet-another-post" title="Yet another post" />
+        <Post slug="second-post" title="Second post" />
+        <Post slug="hello-world" title="Hello, world!" />
+      </li>
+    </ul>
+  </div>
+)
